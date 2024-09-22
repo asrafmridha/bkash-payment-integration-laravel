cde0679f4894e8659bfed7e5b10747c6b159a9fd
asraf mridha <asrafmridha@gmail.com>
2024-09-22 10:32:03 +0600

    Response go to sandbox url

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/bkash/create-payment', [App\Http\Controllers\BkashTokenizePaymentController::class,'createPayment'])->name('bkash-create-payment');
